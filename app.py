1c4488c37ccafd1508a724d90eac87c79dc3be6b
Underemployed <underemployed871@gmail.com>
2025-01-19 10:11:38 +0530

    improved download url

--- app.py
+++ app.py
@@ -143,7 +143,8 @@ class GooglePhotosDownloader:
         
         base_url = item["baseUrl"]
         mime_type = item["mimeType"]
-        download_url = f"{base_url}=d" if "image" in mime_type else f"{base_url}=dv"
+        download_url = f"{base_url}=w{item['mediaMetadata']['width']}-h{item['mediaMetadata']['height']}-d" if "image" in mime_type else f"{base_url}=dv"
+
         
         print(f"Downloading: {filename}")
         response = requests.get(download_url)
